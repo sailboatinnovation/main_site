cdf86bf5178f1a126caee6187512385a0b05c2e4
Junaid Jalal <junaid.mohammed.abdul.jalal@oracle.com>
2025-03-16 14:52:11 -0400

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <div>
+          <div className="text-xl font-medium text-black dark:text-white">Coming Soon!</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
